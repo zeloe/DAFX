a7e415fa1e09571f316ebb69cc78834ae9d0dee5
Zeno Lösch <54742442+zeloe@users.noreply.github.com>
2023-04-08 18:58:27 +0200

    Utility for gain processing

---
+++ Utility/gain_block.h
@@ -0,0 +1,70 @@
+/*
+  ==============================================================================
+
+    gain_block.h
+    Created: 5 Feb 2023 11:39:34pm
+    Author:  Onez
+
+  ==============================================================================
+*/
+
+#pragma once
+/*
+  ==============================================================================
+
+    Sine_Wave.h
+    Created: 17 Jan 2023 5:37:34pm
+    Author:  Onez
+
+  ==============================================================================
+*/
+
+#pragma once
+#include "JuceHeader.h"
+#include "math.h"
+class Gain_Block
+{
+public:
+    Gain_Block()  {};
+    ~Gain_Block() {};
+    
+    void prepare(int blocksize)
+    {
+        temp_gain = 0;
+        bs = blocksize;
+    }
+    
+    void setGain(float gain)
+    {
+        current_gain = gain;
+    }
+    
+    void process(float* input) noexcept
+    {
+        if(temp_gain != current_gain)
+        {
+            // this works for block based processing
+            gain_inc = (current_gain - temp_gain) / bs;
+            for (size_t i = 0; i < bs; ++i)
+            {
+                temp_gain += gain_inc;
+                const float gain = input[i] * temp_gain;
+                input[i] = gain;
+            }
+            temp_gain = current_gain;
+        } else {
+            for (size_t i = 0; i < bs; ++i)
+            {
+                const float gain = input[i] * current_gain;
+                input[i] = gain;
+            }
+        }
+             
+    }
+private:
+    float temp_gain;
+    float current_gain;
+    float gain_inc = 0;
+    size_t bs = 0;
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Gain_Block);
+};
