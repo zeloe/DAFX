9bce1e609997a7f55e132dfbc90b616b4b19fc52
Zeno Lösch <54742442+zeloe@users.noreply.github.com>
2024-03-28 18:45:52 +0100

    used pointer
    -> less to write when params are updated

--- libs/DAFX/Utility/Block_Smoothing.h
+++ libs/DAFX/Utility/Block_Smoothing.h
@@ -16,20 +16,35 @@ public:
         inc = (newParam - currentParam) / bs;
         isSmoothing = true;
     }
-    
-    
-    float smoothing (float& currentParam)
+    void prepare(int maxBlockSize)
     {
-        return currentParam += inc;
+        maxBs = maxBlockSize;
+    }
+    
+    void calcCoeff(float& newParam, float& currentParam)
+    {
+        inc = (newParam - currentParam) / maxBs;
+        isSmoothing = true;
+        this->currentParam = &currentParam;
+        this->newParam = &newParam;
+    }
+    
+    float smoothing ()
+    {
+        return *this->currentParam += inc;
     }
     
     void resetSmoother()
     {
         isSmoothing = false;
+        *this->currentParam = *this->newParam;
     }
     
     bool isSmoothing = false;
     
 private:
     float inc = 0;
+    float maxBs = 0;
+    float* currentParam = nullptr;
+    float* newParam = nullptr;
 };
